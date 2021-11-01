7f2c25d97e84e42deba9556ad2288cd860ca67e4
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 00:12:55 -0600

    Se añade ejemplo safeDelete para borrar parametro en una llamada jerarquica

--- src/main/java/com/example/demo/safedelete/SafeDeleteAParameterForACallHierarchy.java
+++ src/main/java/com/example/demo/safedelete/SafeDeleteAParameterForACallHierarchy.java
@@ -2,8 +2,8 @@ package com.example.demo.safedelete;
 
 public class SafeDeleteAParameterForACallHierarchy {
 
-    private void foo(int i) { bar(i);}
-    private void bar(int i) { baz(i);}
-    private void baz(int i) { }
+    private void foo() { bar();}
+    private void bar() { baz();}
+    private void baz() { }
 
 }
