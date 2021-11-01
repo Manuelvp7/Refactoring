d54791d8104a700b60c89d973b54128d9d5b99f8
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 00:38:12 -0600

    se soluciuonan conflictos

--- src/main/java/com/example/demo/safedelete/SafeDeleteAFunctionForACallHierarchy.java
+++ src/main/java/com/example/demo/safedelete/SafeDeleteAFunctionForACallHierarchy.java
@@ -1,7 +1,4 @@
 package com.example.demo.safedelete;
 
 public class SafeDeleteAFunctionForACallHierarchy {
-    private void foo(int i) { bar(i);}
-    private void bar(int i) { baz(i);}
-    private void baz(int i) { }
 }
--- src/main/java/com/example/demo/safedelete/SafeDeleteAParameterForACallHierarchy.java
+++ src/main/java/com/example/demo/safedelete/SafeDeleteAParameterForACallHierarchy.java
@@ -1,5 +1,7 @@
 package com.example.demo.safedelete;
 
 public class SafeDeleteAParameterForACallHierarchy {
-
+    private void foo() { bar();}
+    private void bar() { baz();}
+    private void baz() { }
 }
