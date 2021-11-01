7e6e970fd73a8f7144cda5533132e415257a3400
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 14:21:37 -0600

    se refactoriza constante

--- src/main/java/com/example/demo/extractconstant/ExtractConstant.java
+++ src/main/java/com/example/demo/extractconstant/ExtractConstant.java
@@ -3,10 +3,12 @@ package com.example.demo.extractconstant;
 import java.util.ArrayList;
 
 public class ExtractConstant {
+    private static final String STRING = "string";
+
     public void method() {
         ArrayList list = new ArrayList();
-        list.add("string");
-        anotherMethod("string");
+        list.add(STRING);
+        anotherMethod(STRING);
     }
 
     private void anotherMethod(String string) {
