dd340736ab96517c95e2d89ffd140757f5391316
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 16:34:40 -0600

    se refactoriza para actual;izar firma del metodo y usar variable del mismo tipo mas cercana

--- src/main/java/com/example/demo/changemethodsignature/AddParamettersToSignature.java
+++ src/main/java/com/example/demo/changemethodsignature/AddParamettersToSignature.java
@@ -1,15 +1,14 @@
 package com.example.demo.changemethodsignature;
 
 public class AddParamettersToSignature {
-    public void myMethod(int value) {
+    public void myMethod(int value, double x) {
 
     }
 
     public class MyOtherClass {
         public void myMethodCall(AddParamettersToSignature myClass) {
             double d = 0.5;
-
-            myClass.myMethod(1);
+            myClass.myMethod(1, d);
         }
     }
 }
