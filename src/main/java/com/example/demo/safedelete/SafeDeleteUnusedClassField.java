73559d338e571b3c2a3f7365b872c6e07f4bd880
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 00:43:07 -0600

    se añade SafeDelete para clase miembro de la clase SafeDeleteClassField

--- src/main/java/com/example/demo/safedelete/SafeDeleteUnusedClassField.java
+++ src/main/java/com/example/demo/safedelete/SafeDeleteUnusedClassField.java
@@ -2,9 +2,6 @@ package com.example.demo.safedelete;
 
 public class SafeDeleteUnusedClassField {
 
-    private final MyProperty myProperty;
-
-    public SafeDeleteUnusedClassField(MyProperty myProperty) {
-        this.myProperty = myProperty;
+    public SafeDeleteUnusedClassField() {
     }
 }
