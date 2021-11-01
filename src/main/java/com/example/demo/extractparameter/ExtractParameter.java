3d7e390f636b93b927bc6997adb8bca89e15f77a
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 14:48:37 -0600

    se añade ejemplo de refactor para extraer parametro

---
+++ src/main/java/com/example/demo/extractparameter/ExtractParameter.java
@@ -0,0 +1,10 @@
+package com.example.demo.extractparameter;
+
+public class ExtractParameter {
+    public static void print() {
+        System.out.println(generateText());
+    }
+    private static String generateText() {
+        return "Hello, World!".toUpperCase();
+    }
+}
