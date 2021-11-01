321a2a6221819249ca8592ea70821ff19ce0b445
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 14:14:11 -0600

    se hace refactor para extracción de constantes

---
+++ src/main/java/com/example/demo/extractconstant/ExtractConstant.java
@@ -0,0 +1,17 @@
+package com.example.demo.extractconstant;
+
+import java.util.ArrayList;
+
+public class ExtractConstant {
+
+    private static final String STRING = "string";
+
+    public void method() {
+        ArrayList list = new ArrayList();
+        list.add(STRING);
+        anotherMethod(STRING);
+    }
+
+    private void anotherMethod(String string) {
+    }
+}
