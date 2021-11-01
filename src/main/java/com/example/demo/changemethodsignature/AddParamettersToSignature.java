51a8732d1f2e804a39ccf9d01a84609437ca8777
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 16:46:47 -0600

    se refactoriza cambio de firma del metodo propagando argumentos a la funcion que invoca

--- src/main/java/com/example/demo/changemethodsignature/AddParamettersToSignature.java
+++ src/main/java/com/example/demo/changemethodsignature/AddParamettersToSignature.java
@@ -1,15 +1,15 @@
 package com.example.demo.changemethodsignature;
 
 public class AddParamettersToSignature {
-    public void myMethod(int value) {
+    public void myMethod(int value, double price) {
 
     }
 
     public class MyOtherClass {
-        public void myMethodCall(AddParamettersToSignature myClass) {
+        public void myMethodCall(AddParamettersToSignature myClass, double price) {
             double d = 0.5;
 
-            myClass.myMethod(1);
+            myClass.myMethod(1, price);
         }
     }
 }
