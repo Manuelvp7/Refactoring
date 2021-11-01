d811bf2891c9d80a4460aea373cf20a9c2f2e0e0
Manuelvp <jmanuelvp@outlook.es>
2021-11-01 10:02:54 -0600

    Se añade ejemplo para extraer metodo

--- src/main/java/com/example/demo/extractmethod/ExtractMethodRefactoring.java
+++ src/main/java/com/example/demo/extractmethod/ExtractMethodRefactoring.java
@@ -1,2 +1,12 @@
-package com.example.demo.extractmethod;public class ExtractMethodRefactoring {
+package com.example.demo.extractmethod;
+
+public class ExtractMethodRefactoring {
+
+    public void method() {
+        int a=1;
+        int b=2;
+        int c=a+b;
+        int d=a+c;
+    }
+
 }
